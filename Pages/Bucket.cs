41c75ffca0a1391dc30eaba41831f3878faf088d
David Looi <david@coinjar.com>
2022-09-29 17:03:56 +1000

    list objects from bucket

---
+++ Pages/Bucket.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Google.Cloud.Storage.V1;
+
+namespace HelloWorldWebapp.Pages;
+
+public class BucketModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public BucketModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public Google.Api.Gax.PagedEnumerable<Google.Apis.Storage.v1.Data.Objects, Google.Apis.Storage.v1.Data.Object> Objects { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var client = StorageClient.Create();
+        var objects = client.ListObjects("dave-proj-bucket");
+        Objects = objects;
+        return Page();
+    }
+}
